9892c9bb088789e8b8f7d20bcee70aafe89241c6
Jin Kwon <onacit@gmail.com>
2024-04-13 13:45:49 +0900

    wip

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/**
+ * Provides packages bound from <a href="https://github.com/spdx/license-list-data">SPDX License List Data</a>.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
 module com.github.jinahya.spdx.licenses {
     requires static lombok;
     exports com.github.jinahya.spdx.license.data.json;
