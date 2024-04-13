38003a941818439182bd92f01493b682b3f914e6
Jin Kwon <onacit@gmail.com>
2024-04-13 12:17:08 +0900

    wip

--- src/main/java/com/github/jinahya/spdx/license/data/json/package-info.java
+++ src/main/java/com/github/jinahya/spdx/license/data/json/package-info.java
@@ -1,1 +1,6 @@
+/**
+ * Bound instances of <a href="https://github.com/spdx/license-list-data">SPDX License List Data</a>.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
 package com.github.jinahya.spdx.license.data.json;
